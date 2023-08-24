8663e8c9566b7cb63b4bcfc3b5e7525e200e6d9c
Rividemeo <76083095+rividemeoo@users.noreply.github.com>
2023-08-24 15:36:11 +0700

    feat: Update some files

--- Python/tests/config.py
+++ Python/tests/config.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 RAND_NUMBER = random.uniform(0.1,9000.99999)
 
-environment = 'test.properties'
+environment = os.environ["PYTHON_ENVIRONMENT"]
 
-if environment == 'engr.properties':
+if environment == 'engr' or environment is None:
     _default_auth_token = 'LxfIUMbcrJWXiwvz0kymfOyTTuIMWPgISmKzTX8njUKYnLxyE/aHzQ=='
     _default_cosmos_db_url = 'https://usazw-ecdbcgn01.documents.azure.com:443/'
     _default_cosmos_db_pk = '8J9ieHGttLnwnW5VElews9SX6jHBpPhasVvMhTBewCIg7U73IXCDJyIjYLMKKMGDVvUzi2eidmDdfGPderZcpQ=='
@@ -17,7 +17,7 @@ if environment == 'engr.properties':
     _default_cosmos_db_container = 'TaxConfig'
     _default_taxapi_url = 'https://usazw-efatapi01.azurewebsites.net/api'
 
-elif environment == 'lab.properties':
+elif environment == 'lab':
     _default_auth_token = 'fXDL6OxA2Befyct2ojCWVPuEAVz4mAEkX3XYHyhCtfZfAzFuNjK2yg=='
     _default_cosmos_db_url = 'https://usazw-lcdbtapi01.documents.azure.com:443/'
     _default_cosmos_db_pk = 'jwwahosw0L9t9palcvcqJ8LhS9o2QMKzBdb9QMSHYXWz8Fiizudj6RGyl81Rf3kNE9xZVOCffZiVZW8qtZxGnQ=='
@@ -25,7 +25,7 @@ elif environment == 'lab.properties':
     _default_cosmos_db_container = 'TaxConfig'
     _default_taxapi_url = 'https://usazw-lfatapi01.azurewebsites.net/api'
 
-elif environment == 'prod.properties':
+elif environment == 'prod':
     _default_auth_token = '89ZumQc7-Pa1lnT5b0vb_op79iQpo91_eXCOif35MK92AzFuIJAX6w=='
     _default_cosmos_db_url = 'https://usaznc-cdbtapi01.documents.azure.com:443/'
     _default_cosmos_db_pk = 'umoMsv7vXWzsBBE3OQoqYShQIl9RyNQP8nHvk4UvEvYmAH7wOWVl93jTzCBh7WELCmwmhXvwp5eHACDb3LKapw=='
